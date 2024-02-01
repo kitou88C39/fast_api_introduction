95d3ca0c4599d34df1daaf5293b9c3c2fbba9259
kitou88C39 <kanfutrooper@icloud.com>
2024-02-01 20:07:30 +0900

    POSTを使ってリクエストボディを使う方法

--- client.py
+++ client.py
@@ -11,3 +11,15 @@ res = requests.get("http://127.0.0.1:8000/items/?skip=1&limit=2")
 
 print(res.status_code)
 print(res.text)
+
+# POSTを使ってリクエストボディを使う方法
+import requests
+
+res = requests.post(
+    "http://127.0.0.1:8000/items/",
+    json={"name": "Tシャツ", "price":2000, "description":"白Tシャツ"},
+
+)
+
+print(res.status_code)
+print(res.text)
