251096819049d3749772ef50f42c65ac345ee2d9
kitou88C39 <kanfutrooper@icloud.com>
2024-02-02 10:05:32 +0900

    ヘッダーとはリクエストやレスポンスにおいて付加情報を伝えるもの

--- client.py
+++ client.py
@@ -42,13 +42,14 @@ import time
 async def sleep_time(sec):
     loop = asyncio.get_running_loop()
     res = await loop.run_in_excutor(
-    None, requests.get,
-    f"http://127.0.0.1:8000/sleep_time/?sec={sec}"
+        None, requests.get, f"http://127.0.0.1:8000/sleep_time/?sec={sec}"
 )
 return res.text
 
 asyncio def main()
     print(f"main開始{time.strftime('%X')}")
+    results = await asyncio.gether(sleep_time(1), sleep_time(2))
+    print(results)
     print(f"main終了{time.strftime('%X')}")
 
 if __name__=="__main__":
